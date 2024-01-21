33251f36ad6e972a3a6f9de13f567fce548f1245
octavian.fusari <octavian.fusari@marconirovereto.it>
2024-01-21 23:45:16 +0100

    aggiunta gsap

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from "../footer/footer.component";
+import gsap from 'gsap';
+import { SplitText } from 'gsap/SplitText';
+
 
 @Component({
     selector: 'app-home',
@@ -13,83 +16,170 @@ export class HomeComponent {
 
 
   ngOnInit(): void {
+    const sections = document.querySelectorAll("section");
+const images = document.querySelectorAll(".bg");
+/* const headings = gsap.utils.toArray(".section-heading"); */
+const outerWrappers:any = gsap.utils.toArray(".outer");
+const innerWrappers:any = gsap.utils.toArray(".inner");
 
+document.addEventListener("wheel", handleWheel);
+document.addEventListener("touchstart", handleTouchStart);
+document.addEventListener("touchmove", handleTouchMove);
+document.addEventListener("touchend", handleTouchEnd);
 
-    let section__up = document.getElementById('section__up') as HTMLElement;
-    let section__center = document.getElementById('section__center') as HTMLElement;
-    let section__center__2 = document.getElementById('section__center__2') as HTMLElement;
-    let footerdiv = document.getElementById('footerdiv') as HTMLElement;
+let listening = false,
+  direction = "down",
+  current: any = "",
+  next = 0;
 
-    let checkPrepos = 0;
-    let checkpos = 0;
+const touch = {
+  startX: 0,
+  startY: 0,
+  dx: 0,
+  dy: 0,
+  startTime: 0,
+  dt: 0
+};
 
-    let counter = 0;
-    let counterPre = 0;
-    
-    window.addEventListener("wheel", function (e) {
-      checkpos = window.scrollY;
-      console.log(e.target)
-      if (checkpos > checkPrepos) {
-        counter += 1
-        if(counter == 0){
-          section__up.style.setProperty("transform","translate3d(0px, 0%, 0px)");
-          section__center.style.setProperty("transform","translate3d(0px, 100%, 0px)");
-          section__center__2.style.setProperty("transform","translate3d(0px, 100%, 0px)");
-          footerdiv.style.setProperty("transform","translate3d(0px, 100%, 0px)");
-        }
-        else if(counter == 1){
-          section__up.style.setProperty("transform","translate3d(0px, 100%, 0px)");
-          section__center.style.setProperty("transform","translate3d(0px, 0%, 0px)");
-          section__center__2.style.setProperty("transform","translate3d(0px, 100%, 0px)");
-          footerdiv.style.setProperty("transform","translate3d(0px, 100%, 0px)");
-        }
-        else if(counter == 2){
-          section__up.style.setProperty("transform","translate3d(0px, 100%, 0px)");
-          section__center.style.setProperty("transform","translate3d(0px, 100%, 0px)");
-          section__center__2.style.setProperty("transform","translate3d(0px, 0%, 0px)");
-          footerdiv.style.setProperty("transform","translate3d(0px, 100%, 0px)");
-        }
-        else if(counter == 3){
-          section__up.style.setProperty("transform","translate3d(0px, 100%, 0px)");
-          section__center.style.setProperty("transform","translate3d(0px, 100%, 0px)");
-          section__center__2.style.setProperty("transform","translate3d(0px, 100%, 0px)");
-          footerdiv.style.setProperty("transform","translate3d(0px, 0%, 0px)");
-        }
-        counterPre = counter
+const tlDefaults = {
+  ease: "slow.inOut",
+  duration: 1.25
+};
+
+/* const splitHeadings = headings.map((heading:any) => {
+  return new SplitText(heading, {
+    type: "chars, words, lines",
+    linesClass: "clip-text"
+  });
+}); */
+
+/* function revealSectionHeading() {
+  return gsap.to(splitHeadings[next].chars, {
+    autoAlpha: 1,
+    yPercent: 0,
+    duration: 1,
+    ease: "power2",
+    stagger: {
+      each: 0.02,
+      from: "random"
+    }
+  });
+} */
+
+gsap.set(outerWrappers, { yPercent: 100 });
+gsap.set(innerWrappers, { yPercent: -100 });
+
+// Slides a section in on scroll down
+function slideIn() {
+  // The first time this function runs, current is undefined
+  if (current !== undefined) gsap.set(sections[current], { zIndex: 0 });
+
+  gsap.set(sections[next], { autoAlpha: 1, zIndex: 1 });
+  gsap.set(images[next], { yPercent: 0 });
+/*   gsap.set(splitHeadings[next].chars, { autoAlpha: 0, yPercent: 100 });
+ */
+  const tl = gsap
+    .timeline({
+      paused: true,
+      defaults: tlDefaults,
+      onComplete: () => {
+        listening = true;
+        current = next;
       }
-      else{
-        setTimeout(() => {
-          if(counter >=0){
-            counter -= 1
-          }
-        }, 1000);
-        if(counter == 0){
-          section__up.style.setProperty("transform","translate3d(0px, 0%, 0px)");
-          section__center.style.setProperty("transform","translate3d(0px, 100%, 0px)");
-          section__center__2.style.setProperty("transform","translate3d(0px, 100%, 0px)");
-          footerdiv.style.setProperty("transform","translate3d(0px, 100%, 0px)");
-        }
-        else if(counter == 1){
-          section__up.style.setProperty("transform","translate3d(0px, 100%, 0px)");
-          section__center.style.setProperty("transform","translate3d(0px, 0%, 0px)");
-          section__center__2.style.setProperty("transform","translate3d(0px, 100%, 0px)");
-          footerdiv.style.setProperty("transform","translate3d(0px, 100%, 0px)");
-        }
-        else if(counter == 2){
-          section__up.style.setProperty("transform","translate3d(0px, 100%, 0px)");
-          section__center.style.setProperty("transform","translate3d(0px, 100%, 0px)");
-          section__center__2.style.setProperty("transform","translate3d(0px, 0%, 0px)");
-          footerdiv.style.setProperty("transform","translate3d(0px, 100%, 0px)");
-        }
-        else if(counter == 3){
-          section__up.style.setProperty("transform","translate3d(0px, 100%, 0px)");
-          section__center.style.setProperty("transform","translate3d(0px, 100%, 0px)");
-          section__center__2.style.setProperty("transform","translate3d(0px, 100%, 0px)");
-          footerdiv.style.setProperty("transform","translate3d(0px, 0%, 0px)");
-        }
+    })
+    .to([outerWrappers[next], innerWrappers[next]], { yPercent: 0 }, 0)
+    .from(images[next], { yPercent: 15 }, 0)
+/*     .add(revealSectionHeading(), 0);
+ */
+  if (current !== undefined) {
+    tl.add(
+      gsap.to(images[current], {
+        yPercent: -15,
+        ...tlDefaults
+      }),
+      0
+    ).add(
+      gsap
+        .timeline()
+        .set(outerWrappers[current], { yPercent: 100 })
+        .set(innerWrappers[current], { yPercent: -100 })
+        .set(images[current], { yPercent: 0 })
+        .set(sections[current], { autoAlpha: 0 })
+    );
+  }
+
+  tl.play(0);
+}
+
+// Slides a section out on scroll up
+function slideOut() {
+  gsap.set(sections[current], { zIndex: 1 });
+  gsap.set(sections[next], { autoAlpha: 1, zIndex: 0 });
+/*   gsap.set(splitHeadings[next].chars, { autoAlpha: 0, yPercent: 100 }); */
+  gsap.set([outerWrappers[next], innerWrappers[next]], { yPercent: 0 });
+  gsap.set(images[next], { yPercent: 0 });
+
+  gsap
+    .timeline({
+      defaults: tlDefaults,
+      onComplete: () => {
+        listening = true;
+        current = next;
       }
-      checkPrepos = checkpos;
-    });
+    })
+    .to(outerWrappers[current], { yPercent: 100 }, 0)
+    .to(innerWrappers[current], { yPercent: -100 }, 0)
+    .to(images[current], { yPercent: 15 }, 0)
+    .from(images[next], { yPercent: -15 }, 0)
+/*     .add(revealSectionHeading(), ">-1") */
+    .set(images[current], { yPercent: 0 });
+}
+
+function handleDirection() {
+  listening = false;
+
+  if (direction === "down") {
+    next = current + 1;
+    if (next >= sections.length) next = 0;
+    slideIn();
+  }
+
+  if (direction === "up") {
+    next = current - 1;
+    if (next < 0) next = sections.length - 1;
+    slideOut();
+  }
+}
+
+function handleWheel(e:any) {
+  if (!listening) return;
+  direction = e.wheelDeltaY < 0 ? "down" : "up";
+  handleDirection();
+}
+
+function handleTouchStart(e:any) {
+  if (!listening) return;
+  const t = e.changedTouches[0];
+  touch.startX = t.pageX;
+  touch.startY = t.pageY;
+}
+
+function handleTouchMove(e:any) {
+  if (!listening) return;
+  e.preventDefault();
+}
+
+function handleTouchEnd(e:any) {
+  if (!listening) return;
+  const t = e.changedTouches[0];
+  touch.dx = t.pageX - touch.startX;
+  touch.dy = t.pageY - touch.startY;
+  if (touch.dy > 10) direction = "up";
+  if (touch.dy < -10) direction = "down";
+  handleDirection();
+}
+
+slideIn();
   }
 
 

3214e981ae67affff26b3f5004d0331ab44842c8
octavian.fusari <octavian.fusari@marconirovereto.it>
2024-03-06 01:49:19 +0100

    aggiunta firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,14 @@ import { HomeComponent } from './components/home/home.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ChiSiamoComponent } from './components/chi-siamo/chi-siamo.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
+import { getPerformance, providePerformance } from '@angular/fire/performance';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { getRemoteConfig, provideRemoteConfig } from '@angular/fire/remote-config';
 
 @NgModule({
     declarations: [
@@ -21,12 +29,38 @@ import { ChiSiamoComponent } from './components/chi-siamo/chi-siamo.component';
         HomeComponent, 
         ChiSiamoComponent
     ],
-    providers: [],
+    providers: [
+        ScreenTrackingService,
+        UserTrackingService
+    ],
     bootstrap: [AppComponent],
     imports: [
         BrowserModule,
         AppRoutingModule,
         FormsModule,
+        provideFirebaseApp(() => initializeApp({
+            "apiKey": "AIzaSyA3nXlRMaaYQP3o30Z_ZtiLWnsioRKv8_U",
+
+            "authDomain": "memoorables-f44d8.firebaseapp.com",
+          
+            "projectId": "memoorables-f44d8",
+          
+            "storageBucket": "memoorables-f44d8.appspot.com",
+          
+            "messagingSenderId": "346707148199",
+          
+            "appId": "1:346707148199:web:4c89fcda38b399607eca82",
+          
+            "measurementId": "G-W9VD82XM5R"
+          
+        })),
+        provideAuth(() => getAuth()),
+        provideAnalytics(() => getAnalytics()),
+        provideFunctions(() => getFunctions()),
+        provideMessaging(() => getMessaging()),
+        providePerformance(() => getPerformance()),
+        provideStorage(() => getStorage()),
+        provideRemoteConfig(() => getRemoteConfig()),
         HttpClientModule,
         ReactiveFormsModule,
         RouterModule.forRoot([
@@ -37,7 +71,8 @@ import { ChiSiamoComponent } from './components/chi-siamo/chi-siamo.component';
             { path: "storie", component: StorieComponent },
             {path:"**", component:ChiSiamoComponent}/* ,
             {path:"chi-siamo", component:ChiSiamoComponent} */
-        ])
+        ]),
+        
     ]
 })
 export class AppModule { }

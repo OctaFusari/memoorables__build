b341ca3680cccb21e58f2fbf1399e1b429c068b1
octavian.fusari <octavian.fusari@marconirovereto.it>
2024-04-05 15:03:28 +0200

    mods

--- src/app/components/storie/storie.component.ts
+++ src/app/components/storie/storie.component.ts
@@ -12,5 +12,26 @@ import { FooterComponent } from "../footer/footer.component";
 export class StorieComponent {
 
     secSto:number = 0;
-    
+    secStoInt:number = 0;
+    ngOnInit(): void {
+        if(this.secStoInt == 1){
+        window.addEventListener('scroll', function () {
+                const img__tre:any = document.querySelector('.img__tre');
+                const img__due:any = document.querySelector('.img__due');
+                const scrollValue = window.scrollY;
+                img__tre.style.transform = `translateY(${scrollValue * 0.003}cm)`;
+                img__due.style.transform = `translateY(${scrollValue * 0.001}cm)`;
+        }); 
+        }
+    }
+
+    checkOpenStoria(){
+        this.secSto = 1
+        if(this.secSto == 1){
+            setTimeout(() => {
+               this.secStoInt = 1
+            }, 3000);
+        }
+    }
 }
+      
